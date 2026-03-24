37a8590bba87a017b2cd4aedc43f31990014b214
Victor S. Miller <victorsmiller@gmail.com>
2026-03-24 09:15:15 -0700

    Removed singletons from tree decomposition, add verbose

--- src/separation/bags.py
+++ src/separation/bags.py
@@ -7,7 +7,22 @@ from typing import Hashable, Tuple, Iterable
 from itertools import combinations
 import networkx as nx
 
-def bag_decomposition(gph: nx.Graph, order: Iterable[Hashable]) -> nx.Graph:
+def bag_decomposition(gph: nx.Graph, order: Iterable[Hashable],
+                      verbose: bool = False) -> nx.Graph:
+    """
+      Given an undirected graph, G, and an ordering of its vertices
+      create the tree decomposition of the graph.
+
+      The tree decomposition of G, is a tree whose nodes are labeled
+      by subsets of the nodes of G, called 'bags'.
+
+      This is a method that was given by Google Gemini.
+
+      Question: It should be the case that if G is connected then T(G) is
+      connected, yet this algorithm doesn't do that.
+
+      
+    """
 
     # Check that the input is compatible
     gnodes = frozenset(gph.nodes)
@@ -17,29 +32,43 @@ def bag_decomposition(gph: nx.Graph, order: Iterable[Hashable]) -> nx.Graph:
 
     ngph = gph.copy()
     bags = {}
-    for ind, node in enumerate(order):
+    norder = order.copy()
+    for node in norder:
 
-        nbrs = ngph.neighbors(node)
-        bags[node] = frozenset([node]).union(set(order[ind + 1:]).intersection(nbrs))
+        nbrs = list(ngph.neighbors(node))
+        if verbose:
+            print(f"{node} -> {nbrs}")
         ngph.remove_node(node)
+        # The bag for this node is the node plus all of its neighbors
+        # which are 'in the future'
+        # This shouldn't be necessary, since we've removed all nodes
+        # before the current one
+        # extra = set(norder).intersection(nbrs)
+        if nbrs:
+            bags[node] = frozenset([node]).union(nbrs)
 
-        # Make the neighbors a clique
+        # Make the future neighbors a clique
         ngph.add_edges_from(combinations(nbrs, 2))
 
 
     # Now create the tree
     tgph = nx.Graph()
 
-    for ind, node in enumerate(order):
+    norder = order.copy()
+    while norder:
+        node = norder[0]
+        norder = norder[1:]
 
-        # Find the bag that share the most vertices with it.
-        tbag = bags[node]
-        cand = max(order[ind + 1:],
-                   default = None,
-                   key = lambda _: len(tbag.intersection(bags[_])))
-        if cand is not None and tbag.intersection(bags[cand]):
+        # Find the bag in the future that share the most vertices with it.
+        if node in bags:
+            tbag = bags[node]
+            if len(tbag) == 1:
+                print(f"{node} is alone!")
+            cand = next((_ for _ in norder if tbag.intersection(bags[_])))
+            if len(tbag.intersection(bags[cand])) == 0:
+                print(f"{node} has no edge")
             tgph.add_edge(tbag, bags[cand])
         else:
-            tgph.add_node(tbag)
+            print(f"The node {node} only has back edges.")
 
     return tgph
